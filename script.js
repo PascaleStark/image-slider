8b538ae196ab0f6ff23065deeb1f180efc3972ee
PascaleStark <pascalestark.ca@gmail.com>
2021-02-03 14:55:05 -0500

    Squares functionality added

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ const nextSlide = function () {
     if (curSlide === maxSlides - 1) curSlide = 0;
   }
   changeSlide(curSlide);
+  activSquare(curSlide);
 };
 
 //Previous slide
@@ -34,6 +35,7 @@ const previousSlide = function () {
     curSlide = maxSlides - 1;
   }
   changeSlide(curSlide);
+  activSquare(curSlide);
 };
 
 //Click buttons event listener
@@ -46,3 +48,41 @@ document.addEventListener("keydown", function (e) {
   if (e.keyCode === 39) nextSlide();
   if (e.keyCode === 37) previousSlide();
 });
+
+//Create the scrolling squares
+const squaresBox = document.querySelector(".slider__squares-box");
+console.log(squaresBox);
+
+//To insert n squares in the box where n is the number of slides present.
+const createSquares = function () {
+  allSlides.forEach((_, i) => {
+    const html = `<div class="slider__square" data-nb="${i}">&nbsp;</div>`;
+    console.log(html);
+
+    squaresBox.insertAdjacentHTML("beforeend", html);
+  });
+};
+createSquares();
+
+squaresBox.addEventListener("click", function (e) {
+  if (e.target.classList.contains("slider__square")) {
+    const slide = e.target.dataset.nb;
+    changeSlide(slide);
+    activSquare(slide);
+  }
+});
+
+//To activate the square, we create a function that accepts a slide as an argument, then adds the active class the element
+const activSquare = function (slide) {
+  document
+    .querySelectorAll(".slider__square")
+    .forEach((square) => square.classList.remove("slider__square--active"));
+  document
+    .querySelector(`.slider__square[data-nb="${slide}"]`)
+    .classList.add("slider__square--active");
+};
+activSquare(0);
+
+// if (htmlNode.dataset.nb === curSlide) {
+//   console.log(htmlNode.dataset.nb);
+// html.classList.add("slider__square--active");}
