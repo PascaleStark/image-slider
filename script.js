52148be0a0e6b3df3dc8ee1dd7f78b8f9994168e
PascaleStark <pascalestark.ca@gmail.com>
2021-02-04 22:16:52 -0500

    responsive images

--- script.js
+++ script.js
@@ -3,10 +3,12 @@
 const allSlides = document.querySelectorAll(".slider__slide");
 const btnRight = document.querySelector(".slider__btn--right");
 const btnLeft = document.querySelector(".slider__btn--left");
+const squaresBox = document.querySelector(".slider__squares-box");
 
 let curSlide = 0;
 const maxSlides = allSlides.length;
 
+//Functions
 //Slide change functionality - translate values
 const changeSlide = function (slide) {
   allSlides.forEach(
@@ -38,22 +40,7 @@ const previousSlide = function () {
   activSquare(curSlide);
 };
 
-//Click buttons event listener
-btnRight.addEventListener("click", nextSlide);
-btnLeft.addEventListener("click", previousSlide);
-
-//keypress event listener
-document.addEventListener("keydown", function (e) {
-  //   console.log(e);
-  if (e.keyCode === 39) nextSlide();
-  if (e.keyCode === 37) previousSlide();
-});
-
-//Create the scrolling squares
-const squaresBox = document.querySelector(".slider__squares-box");
-console.log(squaresBox);
-
-//To insert n squares in the box where n is the number of slides present.
+//Create squares
 const createSquares = function () {
   allSlides.forEach((_, i) => {
     const html = `<div class="slider__square" data-nb="${i}">&nbsp;</div>`;
@@ -63,14 +50,6 @@ const createSquares = function () {
   });
 };
 createSquares();
-
-squaresBox.addEventListener("click", function (e) {
-  if (e.target.classList.contains("slider__square")) {
-    const slide = e.target.dataset.nb;
-    changeSlide(slide);
-    activSquare(slide);
-  }
-});
 
 //To activate the square, we create a function that accepts a slide as an argument, then adds the active class the element
 const activSquare = function (slide) {
@@ -82,6 +61,26 @@ const activSquare = function (slide) {
     .classList.add("slider__square--active");
 };
 activSquare(0);
+
+//Event Listeners
+//Click buttons event listener
+btnRight.addEventListener("click", nextSlide);
+btnLeft.addEventListener("click", previousSlide);
+
+//keypress event listener
+document.addEventListener("keydown", function (e) {
+  //   console.log(e);
+  if (e.keyCode === 39) nextSlide();
+  if (e.keyCode === 37) previousSlide();
+});
+
+squaresBox.addEventListener("click", function (e) {
+  if (e.target.classList.contains("slider__square")) {
+    const slide = e.target.dataset.nb;
+    changeSlide(slide);
+    activSquare(slide);
+  }
+});
 
 // if (htmlNode.dataset.nb === curSlide) {
 //   console.log(htmlNode.dataset.nb);
